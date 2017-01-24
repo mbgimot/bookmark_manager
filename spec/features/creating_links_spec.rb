e9b069ca8a8412137674c7c8da35b497913e3f39
Enrico Graziani <mrenrich84@gmail.com>
2017-01-24 15:51:53 +0000

    User Story 2 As a time-pressed user So that I can save a website I would like to add the site's address and title to my bookmark manager
    Added a /link/new route

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+# As a time-pressed user
+# So that I can save a website
+# I would like to add the site's address and title to my bookmark manager
+feature "Adding links" do
+  scenario "add the site's address and title to my bookmark manager" do
+    visit ('/links/new')
+    url = rand(1000)
+    title = rand(1000)
+    fill_in 'url', with: url
+    fill_in 'title', with: title
+    click_button 'Submit'
+
+    within 'ul#links' do
+      expect(page).to have_content(url)
+      expect(page).to have_content(title)
+    end
+
+    Link.first(title: title, url: url).destroy
+  end
+end
--- spec/features/links_list_spec.rb
+++ spec/features/links_list_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature "Viewing links" do
   scenario "displays a list of existing links" do
-    Link.create(url: 'http://www.google.com', title: 'Google')
+    link = Link.create(url: 'http://www.google.com', title: 'Google')
 
     visit ('/links')
     expect(page.status_code).to eq(200)
@@ -10,6 +10,8 @@ feature "Viewing links" do
     within 'ul#links' do
       expect(page).to have_content("Google")
     end
+
+    link.destroy
     # expect(page).to have_link('', href: 'http://....')
   end
 end
